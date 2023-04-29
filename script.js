7e98ee77515850d95bdb8e957c7c37aae942bea1
Ahmedssami25 <ahmedmohamedsamiie@gmail.com>
2023-04-29 07:00:33 +0200

    shop

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// var div = document.querySelector('.hiddenSection');
+// window.addEventListener('scroll', function(){
+//     if (window.scrollY >=div.offsetTop && window.scrollY <(div.offsetTop + div.offsetHeight)) {
+//         div.style.display= 'none';
+//     } 
+   
+//     else {
+//         div.style.display= 'flex';
+//     }
+// });
